998f2b3adaf4775d267994e1fa2a79a71cbc5db8
Burak <ahmetcanburak54@gmail.com>
2026-02-10 21:37:45 +0300

    echo command started

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,10 +12,22 @@ public class Main {
             System.out.flush();
             input = reader.readLine();
             String[] arr = input.split(" ");
+            boolean wrote = true;
             //System.out.println(Arrays.toString(arr));
             if(arr.length==2 && arr[0] .equals("exit")  && arr[1].equals("0")) {
             	System.exit(0);
-            }else if(input.length()>=1) {
+            }else if(arr.length >=2 && arr[0].equals("echo") && wrote){
+                if (arr.length == 2) {
+                    String echoString = (String)arr[1] + "\n";
+                    System.out.write(echoString.getBytes());
+                    System.out.flush();
+                    wrote = false;
+                } else {
+                    continue;
+                }
+            }
+            
+            else if(input.length()>=1 && wrote) {
             	String output = input + ": Unknown command\n";
             	System.out.write(output.getBytes());
             	System.out.flush();
