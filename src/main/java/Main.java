b5033eae2b08991f24c6a42f188b2d5f486ffb65
Burak <ahmetcanburak54@gmail.com>
2026-02-17 17:32:40 +0300

    Implement external program execution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+// 
 public class Main {
     private static final String[] BUILTIN_COMMANDS = { "echo", "exit", "type", "pwd", "cd" };
 
